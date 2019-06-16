2441b2dc3a88145a29677c6f310f2e69c893e251
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-16 15:14:54 +0100

    Added functionality to view an individual role by id

--- resources/scripts/role.js
+++ resources/scripts/role.js
@@ -11,7 +11,7 @@ async function getAllRoles(display = true) {
     await makeRequest("GET", rolePath + location, "")
         .then(data => {
             if (display) {
-                displayData(data);
+                displayData(data, "deleteRole", "editRole", "newRole");
             }
             result = data;
             return data;
@@ -23,5 +23,27 @@ async function getAllRoles(display = true) {
 
     return result;
 
+}
+
+async function displayRole() {
+    id = (document.getElementById("search-box").value);
+    const location = `/getRole/${id}`;
+
+    //If no id is entered default to get all champions
+    if (id == "") {
+        getAllRoles();
+        return;
+    }
+
+    let role = await makeRequest("GET", rolePath + location, "")
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.log(error);
+        }
+        );
+
+    displayData(role, "deleteRole", "editRole", "newRole");
 }
 

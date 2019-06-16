ba73074d6a282b6a67f07edd5d8af36bb690099b
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-16 14:37:26 +0100

    Added functionality to view all pantheons and view a pantheon by id

--- resources/scripts/pantheon.js
+++ resources/scripts/pantheon.js
@@ -22,3 +22,25 @@ async function getAllPantheons(display = true) {
 
 }
 
+async function displayPantheon() {
+    id = (document.getElementById("search-box").value);
+    const location = `/getPantheon/${id}`;
+
+    //If no id is entered default to get all champions
+    if (id == "") {
+        getAllPantheons();
+        return;
+    }
+
+    panth = await makeRequest("GET", panthPath + location, "")
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.log(error);
+        }
+        );
+
+    displayData(panth, "deletePantheon", "editPantheon", "newPantheon");
+}
+
